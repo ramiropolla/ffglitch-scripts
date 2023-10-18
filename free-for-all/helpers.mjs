a532496a6e1782335442473b7ea001fc2de528a8
Ramiro Polla <ramiro.polla@gmail.com>
2023-10-18 02:15:34 +0200

    add get_*_mvs_range() to helpers.mjs
    Use it with:
      const fwd_mvs_range = get_forward_mvs_range(frame, stream);
    
    fwd_mvs_range will contain an array with the min and max values allowed
    for motion vectors for the given frame.
    
    The range depends on the frame's fcode and the stream's codec.

--- free-for-all/helpers.mjs
+++ free-for-all/helpers.mjs
@@ -27,6 +27,36 @@ export function get_backward_mvs(frame, overflow)
   return get_mvs_internal(frame, overflow, "backward");
 }
 
+function get_mvs_range_internal(frame, stream, which)
+{
+  // bail out if we have no motion vectors
+  let mvs = frame["mv"];
+  if ( !mvs )
+    return;
+
+  const fcode = mvs["fcode"][which];
+  const bit_size = fcode - 1;
+  const shift = (stream.codec == "mpeg2video") ? 5
+              : (stream.codec == "mpeg4")      ? 6
+              :                                  5;
+  const min_val = -(1<<(shift+bit_size-1));
+  const max_val =  (1<<(shift+bit_size-1))-1;
+
+  return [ min_val, max_val ];
+}
+
+/* Get range for forward motion vectors */
+export function get_forward_mvs_range(frame, stream)
+{
+  return get_mvs_range_internal(frame, stream, 0);
+}
+
+/* Get range for backward motion vectors */
+export function get_backward_mvs_range(frame, stream)
+{
+  return get_mvs_range_internal(frame, stream, 1);
+}
+
 /*********************************************************************/
 /* scales value from 'from' range to 'to' range */
 export function scaleValue(value, from_min, from_max, to_min, to_max)
