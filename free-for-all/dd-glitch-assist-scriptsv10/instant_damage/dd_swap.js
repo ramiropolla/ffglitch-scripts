3cee8fbdadfa079291b2113b38315da22b6169ec
Ramiro Polla <ramiro.polla@gmail.com>
2024-10-08 17:02:09 +0200

    dd-glitch-assist-scriptsv10/selective_damage: speedify swap

--- free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_swap.js
+++ free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_swap.js
@@ -1,8 +1,3 @@
-//import('./my_include.js');
-//var n = return_zero();
-//import {return_zero} from './my_include.js';
-//'loadScript('./my_include.js');
-
 // dd_swap.js
 // swaps x and y components of mv
 
@@ -21,23 +16,5 @@ export function glitch_frame(frame)
     // set motion vector overflow behaviour in ffedit to "truncate"
     frame.mv.overflow = "truncate";
 
-    // clear horizontal element of all motion vectors
-    for ( let i = 0; i < fwd_mvs.length; i++ )
-    {
-        // loop through all rows
-        let row = fwd_mvs[i];
-        for ( let j = 0; j < row.length; j++ )
-        {
-            // loop through all macroblocks
-            let mv = row[j];
-
-            // THIS IS WHERE THE MAGIC HAPPENS
-            // SWAP X & Y VECTORS
-            //let t = mv[0]
-            //mv[0] = mv[1];
-            //mv[1] = t;
-            //mv = dd_swap(mv);
-            //mv[0] = return_zero();
-        }
-    }
+    fwd_mvs.swap_hv();
 }
