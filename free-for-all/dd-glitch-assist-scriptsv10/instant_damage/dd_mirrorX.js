b82ba7cfdff251fc6f08e27f07b0468ef1c2000b
Ramiro Polla <ramiro.polla@gmail.com>
2024-10-08 17:32:14 +0200

    dd-glitch-assist-scriptsv10/selective_damage: speedify mirror{X,Y}

--- free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_mirrorX.js
+++ free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_mirrorX.js
@@ -1,9 +1,4 @@
-// dd_mirror_X.js
-
-// clean buffer :
-var buffer = [ ];
-
-var ZOOM = -20;
+// dd_mirrorX.js
 
 export function setup(args)
 {
@@ -20,35 +15,6 @@ export function glitch_frame(frame)
     // set motion vector overflow behaviour in ffedit to "truncate"
     frame.mv.overflow = "truncate";
 
-    // note that we perform a deep copy of the clean motion
-    // vector values before modifying them.
-    let json_str = JSON.stringify(fwd_mvs);
-    let deep_copy = JSON.parse(json_str);
-    // stick em in the buffer
-    buffer = deep_copy;
-
-    var M_H = fwd_mvs.length/2;
-    // VERTICALLY
-    for ( let i = 0; i < fwd_mvs.length; i++ )
-    {
-
-        // loop through all rows
-
-        let row = fwd_mvs[i];
-        var row2 = buffer[i];
-        //var row2 = fwd_mvs[(fwd_mvs.length-1)-i];
-
-        var M_W = row.length/2;
-
-        // HORIZONTALLY
-        for ( let j = 0; j < row.length; j++ )
-        {
-            // loop through all macroblocks
-            let mv = row[j];
-            var mv2 = row2[(row.length - 1) - j];
-            // THIS IS WHERE THE MAGIC HAPPENS
-            mv[0] = 0-mv2[0];
-            mv[1] = mv2[1];
-        }
-    }
+    fwd_mvs.reverse_h();
+    fwd_mvs.mul(-1, 1);
 }
