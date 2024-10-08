e1f828aafacabb28d832cdd0e1594642fb022db2
Ramiro Polla <ramiro.polla@gmail.com>
2024-10-08 15:21:20 +0200

    dd-glitch-assist-scriptsv10/selective_damage: speedify Blur scripts

--- free-for-all/dd-glitch-assist-scriptsv10/selective_damage/dd_BlurSlowest.js
+++ free-for-all/dd-glitch-assist-scriptsv10/selective_damage/dd_BlurSlowest.js
@@ -1,10 +1,10 @@
 // dd_blurSlowest.js
 // apply 20-frame average to the slowest moving mv's
-var LARGEST = 0;
 var SOME_PERCENTAGE = 0.10; // only the slowest % of mv's in each frame get glitched
 
 // global variable holding forward motion vectors from previous frames
-var prev_fwd_mvs = [ ];
+let prev_fwd_mvs = [ ];
+let total_sum;
 
 // change this value to use a smaller or greater number of frmes to average
 var tail_length = 20;
@@ -34,73 +34,42 @@ export function setup(args)
 
 export function glitch_frame(frame)
 {
-    LARGEST = 0;
-    // bail out if we have no motion vectors
-    let mvs = frame["mv"];
-    if ( !mvs )
-        return;
     // bail out if we have no forward motion vectors
-    let fwd_mvs = mvs["forward"];
+    const fwd_mvs = frame.mv?.forward;
     if ( !fwd_mvs )
         return;
 
+    // set motion vector overflow behaviour in ffedit to "truncate"
+    frame.mv.overflow = "truncate";
+
+    const largest = fwd_mvs.largest_sq();
+    const threshold = Math.floor(SOME_PERCENTAGE * largest[2]);
+    const mask = fwd_mvs.compare_lt(threshold);
 
     // update variable holding forward motion vectors from previous
     // frames. note that we perform a deep copy of the clean motion
     // vector values before modifying them.
-    let json_str = JSON.stringify(fwd_mvs);
-    let deep_copy = JSON.parse(json_str);
+    const deep_copy = fwd_mvs.dup();
     // push to the end of array
     prev_fwd_mvs.push(deep_copy);
-    // drop values from earliest frames to always keep the same tail
-    // length
+
+    // initialize total_sum to a [0,0] MV2DArray.
+    if ( !total_sum )
+        total_sum = new MV2DArray(fwd_mvs.width, fwd_mvs.height);
+
+    // update total_sum by removing the motion vector values from the
+    // oldest frame and adding the values from the current frame.
     if ( prev_fwd_mvs.length > tail_length )
-        prev_fwd_mvs = prev_fwd_mvs.slice(1);
-
-    // bail out if we still don't have enough frames
-    if ( prev_fwd_mvs.length != tail_length )
-        return;
-
-       // 1st loop - find the fastest mv
-       // this ends-up in LARGEST as the square of the hypotenuse (mv[0]*mv[0]) + (mv[1]*mv[1])
-    let W = fwd_mvs.length;
-    for ( let i = 0; i < fwd_mvs.length; i++ )
     {
-        let row = fwd_mvs[i];
-        // rows
-        let H = row.length;
-        for ( let j = 0; j < row.length; j++ )
-        {
-            // loop through all macroblocks
-            let mv = row[j];
-
-            // THIS IS WHERE THE MEASUREMENT HAPPENS
-            var this_mv = (mv[0] * mv[0])+(mv[1] * mv[1]);
-            if ( this_mv > LARGEST){
-                LARGEST = this_mv;
-            }
-        }
+        total_sum.sub(prev_fwd_mvs[0]);
+        prev_fwd_mvs = prev_fwd_mvs.slice(1);
     }
+    total_sum.add(deep_copy);
 
-    // then find those mv's which are bigger than SOME_PERCENTAGE of LARGEST
-    // and then replace them with the average mv from the last n frames
-    for ( let i = 0; i < fwd_mvs.length; i++ )
-        {
-            let row = fwd_mvs[i];
-            // rows
-            let H = row.length;
-            for ( let j = 0; j < row.length; j++ )
-            {
-                // loop through all macroblocks
-                let mv = row[j];
-
-                // THIS IS WHERE THE MAGIC HAPPENS
-                var this_mv = (mv[0] * mv[0])+(mv[1] * mv[1]);
-                if (this_mv < (LARGEST * SOME_PERCENTAGE)){
-
-                     mv[0] = average_mv(mv, i, j, tail_length, 0);
-                    mv[1] = average_mv(mv, i, j, tail_length, 1);
-                }
-            }
+    // set new values for current frame to (total_sum / tail_length)
+    if ( prev_fwd_mvs.length == tail_length )
+    {
+        fwd_mvs.assign(total_sum, mask);
+        fwd_mvs.div(MV(tail_length, tail_length), mask);
     }
 }
