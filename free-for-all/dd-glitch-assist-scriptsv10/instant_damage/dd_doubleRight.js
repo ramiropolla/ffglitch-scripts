bfbdfe3c4f865d37fc2ceab591bcbfc5e9328ac8
Ramiro Polla <ramiro.polla@gmail.com>
2024-10-08 17:58:18 +0200

    dd-glitch-assist-scriptsv10/selective_damage: speedify double{Right,Speed}

--- free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_doubleRight.js
+++ free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_doubleRight.js
@@ -1,4 +1,4 @@
-// dd_double_right.js
+// dd_doubleRight.js
 // every mv on the right of the frame is multiplied by 2
 
 export function setup(args)
@@ -16,24 +16,7 @@ export function glitch_frame(frame)
     // set motion vector overflow behaviour in ffedit to "truncate"
     frame.mv.overflow = "truncate";
 
-    // clear horizontal element of all motion vectors
-    for ( let i = 0; i < fwd_mvs.length; i++ )
-    {
-        let row = fwd_mvs[i];
-        let MID_POINT = row.length/2;
-        // loop through all rows
-
-        for ( let j = 0; j < row.length; j++ )
-        {
-            // loop through all macroblocks
-            let mv = row[j];
-
-            // THIS IS WHERE THE MAGIC HAPPENS
-            // DOUBLESPEED THE RIGHT HAND SIDE OF THE FRAME
-            if(j > MID_POINT){
-                mv[0] = mv[0] * 2;
-                mv[1] = mv[1] * 2;
-            }
-        }
-    }
+    const MID_POINT = fwd_mvs.width / 2;
+    const subarray = fwd_mvs.subarray([MID_POINT, 0]);
+    subarray.mul(2, 2);
 }
--- free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_doubleSpeed.js
+++ free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_doubleSpeed.js
@@ -1,4 +1,4 @@
-// dd_double_right.js
+// dd_doubleSpeed.js
 // every mv is multiplied by 2
 
 export function setup(args)
@@ -16,20 +16,5 @@ export function glitch_frame(frame)
     // set motion vector overflow behaviour in ffedit to "truncate"
     frame.mv.overflow = "truncate";
 
-    // clear horizontal element of all motion vectors
-    for ( let i = 0; i < fwd_mvs.length; i++ )
-    {
-        // loop through all rows
-        let row = fwd_mvs[i];
-        for ( let j = 0; j < row.length; j++ )
-        {
-            // loop through all macroblocks
-            let mv = row[j];
-
-            // THIS IS WHERE THE MAGIC HAPPENS
-
-            mv[0] = mv[0]*2; // this sets the horizontal motion vector to 2x
-            mv[1] = mv[1]*2; // this sets the vertical motion vector to 2x
-        }
-    }
+    fwd_mvs.mul(2, 2);
 }
