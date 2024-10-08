e2234d32dfea1ff57c27a43385872ca13313fc76
Ramiro Polla <ramiro.polla@gmail.com>
2024-10-08 18:57:48 +0200

    dd-glitch-assist-scriptsv10/selective_damage: speedify delay

--- free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_delay.js
+++ free-for-all/dd-glitch-assist-scriptsv10/instant_damage/dd_delay.js
@@ -2,18 +2,20 @@
 // works kinda like an audio delay
 // stacks the previous n frames into a buffer
 
-// global variable holding forward motion vectors from previous frames
-var prev_fwd_mvs = [ ];
-
-// change these values to use a smaller or greater number of frames to
-// perform the average of motion vectors
-
 // try making the delay long enough to overlap an edit in the content ...
-var delay = 20;
+let delay = 20;
+
+// global variable holding forward motion vectors from previous frames
+let prev_fwd_mvs = [ ];
 
 export function setup(args)
 {
     args.features = [ "mv" ];
+
+    // Pass "-sp <value>" in the command line, where <value> is an
+    // integer.
+    if ( "params" in args )
+        delay = args.params;
 }
 
 export function glitch_frame(frame)
@@ -29,8 +31,7 @@ export function glitch_frame(frame)
     // update variable holding forward motion vectors from previous
     // frames. note that we perform a deep copy of the clean motion
     // vector values before modifying them.
-    let json_str = JSON.stringify(fwd_mvs);
-    let deep_copy = JSON.parse(json_str);
+    let deep_copy = fwd_mvs.dup();
     // push to the end of array
     prev_fwd_mvs.push(deep_copy);
     // drop values from earliest frames to always keep the same tail
@@ -42,34 +43,5 @@ export function glitch_frame(frame)
     if ( prev_fwd_mvs.length != delay )
         return;
 
-    // replace all motion vectors of current frame with an average
-    // of the motion vectors from the previous 10 frames
-    for ( let i = 0; i < fwd_mvs.length; i++ )
-    {
-        // loop through all rows
-        let row = fwd_mvs[i];
-        let delay_row = prev_fwd_mvs[0][i];
-        let insert_row = prev_fwd_mvs[delay-1][i];
-
-        for ( let j = 0; j < row.length; j++ )
-        {
-            // loop through all macroblocks
-            let mv = row[j];
-            let dmv = delay_row[j];
-            let imv = insert_row[j];
-            // THIS IS WHERE THE MAGIC HAPPENS
-
-        // temp copy of the incoming vectors
-            let x = mv[0];
-            let y = mv[1];
-        // pull their replacements out of the buffer
-            mv[0] = dmv[0];
-            mv[1] = dmv[1];
-        // feedback the 'old' with the 'new' for next time
-            imv[0] = x;
-            imv[1] = y;
-        // rinse and repeat
-
-        }
-    }
+    fwd_mvs.assign(prev_fwd_mvs[0]);
 }
